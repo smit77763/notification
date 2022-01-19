de53375d5c8eae4e897145dfeb1598a253e85797
sashank parmar <parmarsatvan@gmail.com>
2022-01-19 14:53:32 +0530

    L and l ma farak hoy smit shah

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,9 +8,12 @@
  */
 
 import React from 'react';
-import {View, Text, Flatlist} from 'react-native';
+import {Component} from 'react';
+import {View, Text} from 'react-native';
 
-export default class App extends React.Component {
+import {FlatList} from 'react-native';
+
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -55,56 +59,16 @@ export default class App extends React.Component {
       console.log(e);
     }
   }
-  // async componentDidUpdate(){
-  //   // let data=this.state.data;
-  //   // let mid=this.state.mid;
 
-  //   try{
-
-  //   await fetch('http://contest-test-2.herokuapp.com/usernotifi/getByMid',{
-  //     method:'post',
-  //     headers:{
-  //       'Accept':'application/json',
-  //       'Content-Type':'application/json'
-  //     },
-  //     body:JSON.stringify({
-  //       "uid":"600fb97efe981e0004891abb",
-  //       "mid":this.state.mid
-  //    }
-  //    )
-  //   }).then((result)=>{
-  //     result.json().then((resp)=>{
-  //       //resp is array
-  //       // console.warn('response : ',resp);
-  //       this.state.data=[...this.state.data,...resp];
-  //       this.setState({data:[...this.state.data,...resp]})
-  //       // this.state.mid=this.state.data.length;
-  //       // this.setState({mid:this.state.data.length})
-  //       console.warn('data : ',this.state.data,'Length : ',this.state.data.length,'mid : ',this.state.mid);
-
-  //     })
-  //   });
-  // }
-  // catch(e){
-  //   console.log(e);
-
-  // }
-
-  // }
   render() {
     return (
       <View>
-        {/* <Text>Hello Again</Text> */}
-        <Flatlist
+        <FlatList
           data={this.state.data}
-          // renderItem={({item}) => {
-          //   return (<View><Text>HEll</Text></View>);
-          // }}
-          // renderItem={<Text>Hello</Text>}
-          // onEndReached
+          renderItem={({item}) => {
+            return <Text style={{color: 'white'}}>{item.qty} </Text>;
+          }}
         />
-
-        {/* <Text>{this.state.mid}</Text> */}
       </View>
     );
   }
