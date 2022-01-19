bb6203bb181b637b5ccfca222c1e3d04447301d1
Smit Shah <smit77763@gmail.com>
2022-01-19 19:15:45 +0530

    third commit

--- App.js
+++ App.js
@@ -1,82 +1,88 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import {FlatList} from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mid: 0,
-      data: [],
-    };
-  }
+const App = () => {
+  const [mid, setMid] = useState(0);
+  const [data, setData] = useState([]);
 
-  async componentDidMount() {
-    // let data=this.state.data;
-    // let mid=this.state.mid;
+  const callApi = () => {
+    fetch('http://contest-test-2.herokuapp.com/usernotifi/getByMid', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        uid: '600fb97efe981e0004891abb',
+        mid: mid,
+      }),
+    }).then(result => {
+      result.json().then(resp => {
+        //resp is array
+        // console.warn('response : ',resp);
+        // this.state.data = [...this.state.data, ...resp];
+        if (resp.data == '') {
+          console.log('no data fond bolte');
+        }
 
-    try {
-      await fetch('http://contest-test-2.herokuapp.com/usernotifi/getByMid', {
-        method: 'post',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          uid: '600fb97efe981e0004891abb',
-          mid: this.state.mid,
-        }),
-      }).then(result => {
-        result.json().then(resp => {
-          //resp is array
-          // console.warn('response : ',resp);
-          this.state.data = [...this.state.data, ...resp];
-
-          // this.state.mid=this.state.data.length;
-          console.warn(
-            'data : ',
-            this.state.data,
-            'Length : ',
-            this.state.data.length,
-            'mid : ',
-            this.state.mid,
-          );
-        });
+        setData([...data, ...resp]);
+        // this.state.mid=this.state.data.length;
+        console.warn('data : ', data, 'Length : ', data?.length, 'mid : ', mid);
       });
-    } catch (e) {
-      console.log(e);
-    }
-  }
+    });
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello 1</Text>
+  useEffect(() => {
+    callApi();
+  }, [mid]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.FlatList}>
         <FlatList
-          data={this.state.data}
-          renderItem={data => <Text>${data.type}</Text>}
-          onEndReached={() => {
-            this.setState({mid: this.data.length});
+          data={data}
+          renderItem={({item: data, index}) => {
+            console.log(' IN Flatlist');
+            return (
+              <Text style={styles.text}>
+                {index + 1} ){data.type} : {data.qty}
+              </Text>
+            );
           }}
-          style={styles.FlatList}
+          onEndReached={() => {
+            setMid(data?.length);
+          }}
+          onEndReachedThreshold={0.5}
+          // style={styles.FlatList}
         />
-        <Text>Hello 2</Text>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     color: 'black',
-    backgroundColor: 'white',
+    // backgroundColor: 'white',
+    paddingVertical: 70,
+    borderBottomWidth: 2,
+    // alignSelf: 'center',
   },
   FlatList: {
     backgroundColor: 'yellow',
+    borderWidth: 3,
+    flex: 0.6,
+
+    // padding: 50,
   },
 });
+
+export default App;
